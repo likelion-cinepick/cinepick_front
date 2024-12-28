4d0dfe91ba328be1fb6180bb78fa09662a0d3f66
sinyoung6491 <sinyoung6491@gmail.com>
2024-12-28 15:58:17 +0900

    [feat]분위기 키워드 추가

--- src/components/KeywordMood.jsx
+++ src/components/KeywordMood.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
 const KeywordMood = () => {
-  return (
-    <div className='mood_components'>
-        <p>🔥</p>
-        <p>따뜻한</p>
-    </div>
-  )
-}
+    const moods = [
+      { icon: '🔥', text: '따뜻한' },
+      { icon: '💭', text: '생각이 필요한' },
+      { icon: '🎶', text: '경쾌한' },
+      { icon: '🌟', text: '재밌는' }, 
+      { icon: '💌', text: '달달한' },
+      { icon: '🎬', text: '실제의' },
+      { icon: '🩸', text: '무서운' },
+      { icon: '🎀', text: '보기 좋은' },
+      { icon: '💥', text: '짧고 강렬한' },
+      { icon: '🍀', text: '희망찬' },
+      { icon: '💡', text: '교훈 있는' },
+      { icon: '⚒️', text: '잔혹한' },
+      { icon: '🧩', text: '모호한' },
+      { icon: '🔗', text: '심오한' },
+      { icon: '💎', text: '용감한' },
+    ];
+  
+    return (
+      <div className="mood_components">
+        {moods.map((mood, index) => (
+          <div key={index} className="item">
+            <p>{mood.icon}</p>
+            <p>{mood.text}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
 export default KeywordMood
