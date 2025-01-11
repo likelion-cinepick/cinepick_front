2c6562cdcbb31c1c64661b0cff0d6df68498cf18
yuchae Park <20232293@sungshin.ac.kr>
2025-01-11 20:27:04 +0900

    feat: movie detail, recommend page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './page/Home';
 import './assets/scss/style.scss';
+import MovieDetail from './page/MovieDetail';
+import MoreRecommend from './page/MoreRecommend';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/movie/detail' element={<MovieDetail/>} />
+        <Route path='/movie/detail/more' element={<MoreRecommend/>} />
       </Routes>
     </BrowserRouter>
   )
