689204b49c6d1116d84ab2014eecee96eada186d
yuchae。。。 <163510284+starsshootingthemoon@users.noreply.github.com>
2025-01-12 20:23:57 +0900

    Revert "feat: movie detail, recommend page"

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import Re_nickname from './page/Re_nickname';
 import Re_mood from './page/Re_mood';
 import Re_mbti from './page/Re_mbti';
 import './assets/scss/style.scss';
-import MovieDetail from './page/MovieDetail';
-import MoreRecommend from './page/MoreRecommend';
 import TestHome from './page/TestHome';
 import TestQ1 from './page/TestQ1';
 import TestLoading from './page/TestLoading';
@@ -25,8 +23,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/movie/detail' element={<MovieDetail/>} />
-        <Route path='/movie/detail/more' element={<MoreRecommend/>} />
         <Route path='/Home' element={<TestHome/>} />
         <Route
           path="/TestQ1"
